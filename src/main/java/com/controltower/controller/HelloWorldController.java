c2cf5910d3ea3fc5319de1e03a39eb2444da5a22
AdilsonAr <cycles.250@gmail.com>
2021-08-16 22:49:05 -0600

    hello world test

---
+++ src/main/java/com/controltower/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.controltower.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.controltower.model.KeyValue;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+	@GetMapping
+	public ResponseEntity<?> get(){
+		KeyValue keyValue=new KeyValue();
+		keyValue.setKeyAttribute("message");
+		keyValue.setValAttribute("Hello world!");
+		return new ResponseEntity<>(keyValue, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/controltower/model/KeyValue.java
@@ -0,0 +1,9 @@
+package com.controltower.model;
+
+import lombok.Data;
+
+@Data
+public class KeyValue {
+	private String keyAttribute;
+	private String valAttribute;
+}
