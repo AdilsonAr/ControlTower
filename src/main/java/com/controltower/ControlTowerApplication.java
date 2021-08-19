e86b00f4d2c2e99803890fd1e5ec39a56a08b611
AdilsonAr <cycles.250@gmail.com>
2021-08-18 22:50:21 -0600

    spring boot console application setup

--- src/main/java/com/controltower/ControlTowerApplication.java
+++ src/main/java/com/controltower/ControlTowerApplication.java
@@ -1,13 +1,26 @@
 package com.controltower;
 
+import java.util.Scanner;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ControlTowerApplication {
+public class ControlTowerApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlTowerApplication.class, args);
 	}
+	
+	@Override
+    public void run(String... args) {
+		Scanner scanner= new Scanner(System.in);
+        System.out.println("hello world!");
+        System.out.println("tell me your name: ");
+        String name=scanner.next();
+        System.out.println("well, have a nice day " + name);
+        scanner.close();
+    }
 
 }
