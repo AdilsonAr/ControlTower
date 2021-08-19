00e03c5ad5a11d504dbab233b81b800beadbbb87
AdilsonAr <cycles.250@gmail.com>
2021-08-19 16:15:37 -0600

    preparing to merge

--- src/main/java/com/controltower/ControlTowerApplication.java
+++ src/main/java/com/controltower/ControlTowerApplication.java
@@ -1,18 +1,9 @@
 package com.controltower;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import com.controltower.controller.FlightController;
-
 public class ControlTowerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		
-		
 	}
 	
 }
