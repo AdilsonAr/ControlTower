648e17e97387463668aa25aa2003ab5861f3366a
Ruben Vigil <ruben.vigil24@gmail.com>
2021-08-19 20:57:22 +0100

    fixed sheets reader service

--- src/main/java/com/controltower/service/SheetsReaderService.java
+++ src/main/java/com/controltower/service/SheetsReaderService.java
@@ -5,19 +5,15 @@ import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.controltower.configuration.SheetsProviderConfiguration;
+import com.controltower.configuration.GoogleServicesProvider;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.BatchGetValuesResponse;
 import com.google.api.services.sheets.v4.model.Sheet;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
-@Service
+
 public class SheetsReaderService {
 
-	@Autowired
 	private static Sheets sheets;
 
 	private static String getIdFromUrl(String url) {
@@ -42,7 +38,7 @@ public class SheetsReaderService {
 		List<String> ranges = Arrays.asList("A2:C1000");
 		String spreadSheetId = getIdFromUrl(url);
 		try {
-			sheets = new SheetsProviderConfiguration().getSheets();
+			sheets = GoogleServicesProvider.getSheets();
 			BatchGetValuesResponse readResult = sheets.spreadsheets().values().batchGet(spreadSheetId).setRanges(ranges)
 					.execute();
 
