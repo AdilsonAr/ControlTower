d7271bee0b61b9aab79be009d10ffbc6b01d9287
AdilsonAr <cycles.250@gmail.com>
2021-09-09 22:35:00 -0600

    v2

--- src/main/java/com/controltower/ControlTowerApplication.java
+++ src/main/java/com/controltower/ControlTowerApplication.java
@@ -11,11 +11,10 @@ import com.controltower.views.printers.PrinterConsole;
 public class ControlTowerApplication {
 
 	public static void main(String[] args) {
-		/*Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		Printer output = new PrinterConsole();
 		View startMenu = new MainMenuView(input, output);
-		startMenu.show();*/
-		System.out.println(WeatherService.getOneCityWeather("Miami"));
+		startMenu.show();
 	}
 
 }
