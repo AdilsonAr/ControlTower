827af5d6cdbb8b71fb977808817f107961e4d99c
AdilsonAr <cycles.250@gmail.com>
2021-09-09 22:07:12 -0600

    test cases aproved

--- src/test/java/com/controltower/TestCases.java
+++ src/test/java/com/controltower/TestCases.java
@@ -1,8 +1,13 @@
 package com.controltower;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
+
+import org.junit.Test;
 
 import com.controltower.dao.FlightDao;
 import com.controltower.model.Aircraft;
@@ -10,78 +15,59 @@ import com.controltower.model.flight.Flight;
 import com.controltower.service.AircraftService;
 import com.controltower.service.FlightService;
 import com.controltower.service.SheetsReaderService;
-import com.controltower.service.WeatherService;
-
-class Test {
-
-	/*
-	@org.junit.jupiter.api.Test
-	void test() {
-		
+public class TestCases {
+	@Test
+	public void test() {
 		AircraftService service= new AircraftService();
 		List<Aircraft> list=service.getAllowed();
 		list.forEach(x->{
 			System.out.println(x.getModel());
 		});
-		
+		assertTrue(list.size()>0);
 	}
 	
-	@org.junit.jupiter.api.Test
-	void test2() {
+	@Test
+	public void test2() {
 		FlightDao dao=new FlightDao();
-		Flight flight=dao.readById(1);
+		Flight flight=dao.readById(2);
 		System.out.println(flight.getCurrentStateText());
 		flight.setCurrentStateText("LANDED");
 		dao.update(flight);
-		System.out.println((dao.readById(1)).getCurrentStateText());
 		
+		Flight newFlight=dao.readById(1);
+		assertTrue(newFlight.getCurrentStateText().equals("LANDED"));
 	}
 	
-	@org.junit.jupiter.api.Test
-	void test3() {
-		
+	@Test
+	public void test3() {
 		FlightDao dao=new FlightDao();
 		Flight flight=dao.readByNumber("YH 56");
-		System.out.println(flight.getFlightNumber());
-		System.out.println(flight.getIdFlight());
-		
+		assertNotNull(flight);
+		assertTrue(flight.getFlightNumber().equals("YH 56"));
 	}
 	
-	@org.junit.jupiter.api.Test
-	void test4() {
-		
+	@Test
+	public void test4() {
 		FlightService flightService=new FlightService();
 		System.out.println(flightService.readByFlightNumber("JK 55").getCurrentStateText());
 		flightService.cancelFlight("JK 55", "Wheather was bad");
 		System.out.println(flightService.readByFlightNumber("JK 55").getCurrentStateText());
-		
 	}
 	
-	*/
-	
-	/*
-	@org.junit.jupiter.api.Test
-	void test5() {
-		
+	@Test
+	public void test5() {
 		FlightService flightService=new FlightService();
 		System.out.println(flightService.readById(10).getCurrentStateText());
 		flightService.landFlight("JK 55", "Wheather was god");
 		System.out.println(flightService.readById(10).getCurrentStateText());
-		
 	}
 	
-	@org.junit.jupiter.api.Test
-	void test6() {
-		
-		System.out.println(WeatherService.getOneCityWeather("San Salvador"));
-		
-	}
-	
-	@org.junit.jupiter.api.Test
-	void test7() throws IOException, GeneralSecurityException {
+	@Test
+	public void test7() throws IOException, GeneralSecurityException {
 		SheetsReaderService  sheetsReaderService=new SheetsReaderService();
-		String m=sheetsReaderService.readReportFromUrl("https://docs.google.com/spreadsheets/d/16A-0NByteqdkniz4Tr6LRao5fjdeBrBbCUxBA6RYbwQ/edit?usp=sharing");
-		System.out.println(m);
+		String message=sheetsReaderService.readReportFromUrl("https://docs.google.com/spreadsheets/d/16A-0NByteqdkniz4Tr6LRao5fjdeBrBbCUxBA6RYbwQ/edit?usp=sharing");
+		System.out.println(message);
+		assertNotNull(message);
+		assertTrue(!message.isBlank());
 	}
-*/
 }
